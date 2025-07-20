07ef030568aa5f039719f15eb85b2f9877f31acc
WakanaNunomiya <kifxvau743@gmail.com>
2025-07-21 02:28:40 +0900

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,27 @@
-import express, { Express, Request, Response } from 'express';
-import cors from 'cors';
-import { createServer } from 'http';
-import { Server } from 'socket.io';
-import datasource from './datasource';
-import workSpaceController from './module/workspaces/workspace.controller';
-import authController from './module/auth/auth.controller';
-import setCurrentUser from './middleware/set-current-user';
-import channelController from './module/channels/channel.controller';
-import messageController from './module/messages/message.controller';
-import accountController from './module/account/account.controller';
-import workspaceUserController from './module/workspace-users/workspace-user.controller';
-import userController from './module/users/user.controller';
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import datasource from "./datasource";
+import workSpaceController from "./module/workspaces/workspace.controller";
+import authController from "./module/auth/auth.controller";
+import setCurrentUser from "./middleware/set-current-user";
+import channelController from "./module/channels/channel.controller";
+import messageController from "./module/messages/message.controller";
+import accountController from "./module/account/account.controller";
+import workspaceUserController from "./module/workspace-users/workspace-user.controller";
+import userController from "./module/users/user.controller";
 
-require('dotenv').config();
-const port = 8888;
+require("dotenv").config();
+const port = process.env.PORT || 8888;
 const app: Express = express();
 const httpServer = createServer(app);
+// 環境変数からオリジンを取得
+const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || "http://localhost:5173";
 const io = new Server(httpServer, {
   cors: {
-    origin: '*', // 本番環境では適切なオリジンに変更してください
-    methods: ['GET', 'POST'],
+    origin: CLIENT_ORIGIN,
+    methods: ["GET", "POST"],
   },
 });
 
@@ -28,26 +30,26 @@ export { io };
 
 // JSONミドルウェアの設定
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: CLIENT_ORIGIN }));
 app.use(setCurrentUser);
 
 // 静的ファイル配信の設定
-app.use('/uploads', express.static('uploads'));
+app.use("/uploads", express.static("uploads"));
 
 // ルートの設定
-app.use('/auth', authController);
-app.use('/account', accountController);
-app.use('/workspaces', workSpaceController);
-app.use('/channels', channelController);
-app.use('/messages', messageController);
-app.use('/workspace-users', workspaceUserController);
-app.use('/users', userController);
+app.use("/auth", authController);
+app.use("/account", accountController);
+app.use("/workspaces", workSpaceController);
+app.use("/channels", channelController);
+app.use("/messages", messageController);
+app.use("/workspace-users", workspaceUserController);
+app.use("/users", userController);
 
-io.on('connection', (socket) => {
-  console.log('クライアント接続: ', socket.id);
+io.on("connection", (socket) => {
+  console.log("クライアント接続: ", socket.id);
 
-  socket.on('disconnect', () => {
-    console.log('クライアント切断: ', socket.id);
+  socket.on("disconnect", () => {
+    console.log("クライアント切断: ", socket.id);
   });
 });
 
@@ -60,6 +62,6 @@ datasource
   })
   .catch((error) => console.error(error));
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('hello world');
+app.get("/", (req: Request, res: Response) => {
+  res.send("hello world");
 });
